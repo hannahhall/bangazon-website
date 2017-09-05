ffec02b69743995cc41e0db630565eeffd82881b
Hannah Hall <hannahkhall13@gmail.com>
2017-09-05 18:45:17 -0500

    product migrations

--- Migrations/20170905192931_ProductUpdate.Designer.cs
+++ Migrations/20170905192931_ProductUpdate.Designer.cs
@@ -8,9 +8,10 @@ using Bangazon.Data;
 namespace BangazonAuth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170905192931_ProductUpdate")]
+    partial class ProductUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.3");
---
+++ Migrations/20170905192931_ProductUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BangazonAuth.Migrations
+{
+    public partial class ProductUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "LocalDelivery",
+                table: "Product",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Product",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalDelivery",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Product");
+        }
+    }
+}
