97deb402c7da0b693360e03b1280da3c65c1caf1
Hannah Hall <hannahkhall13@gmail.com>
2017-09-07 12:20:21 -0500

    added date to order

--- Migrations/20170906193557_orderdate.Designer.cs
+++ Migrations/20170906193557_orderdate.Designer.cs
@@ -8,9 +8,10 @@ using Bangazon.Data;
 namespace BangazonAuth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170906193557_orderdate")]
+    partial class orderdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.3");
